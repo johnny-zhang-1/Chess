12ecef166598b3eb448bf93c574b1881bcc9ab65
Nathan-Gu2024 <83109479+Nathan-Gu2024@users.noreply.github.com>
2021-05-21 10:53:26 -0700

    Update MyPanel.java

--- src/chess/MyPanel.java
+++ src/chess/MyPanel.java
@@ -8,10 +8,14 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel{
-
+    
+    
     private BufferedImage img;
+    
+    //sets the correct directory to look for the image of the board
     public static String currentDir = System.getProperty("user.dir");
 
+    //assigns the board image into the variable img
     public MyPanel(){
         try {
             img = ImageIO.read(new File(currentDir + "/assets/board.jpg" ));
@@ -20,7 +24,8 @@ public class MyPanel extends JPanel{
         }
 
     }    
-
+    
+    //draws out the chess board
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
